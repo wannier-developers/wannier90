3d5dc8823ab55fae6d347758c2cb18e0569d8088
Jonathan Yates <jonathan.yates@materials.ox.ac.uk>
2006-04-22 02:00:36 +0000

    Fix bug - testing equality of a logical

--- src/parameters.F90
+++ src/parameters.F90
@@ -1,31 +1,5 @@
 !-*- mode: F90; mode: font-lock; column-number-mode: true -*-!
 !                                                            !
-!                       WANNIER90                            !
-!                                                            !
-!          The Maximally-Localised Generalised               !
-!                Wannier Functions Code                      !
-!                                                            !
-!        Authors:                                            !
-!              Arash A. Mostofi   (MIT)                      !
-!              Jonathan R. Yates  (LBNL and UC Berkeley)     !
-!                                                            !
-!  Please cite                                               !
-!                                                            !
-!  [ref] A. A. Mostofi, J. R. Yates, N. Marzari, I. Souza    !
-!        and D. Vanderbilt, http://www.wannier.org/          !
-!                                                            !
-!  in any publications arising from the use of this code.    !
-!                                                            !
-!  Wannier90 is based on routines written by N. Marzari,     !
-!  I. Souza and D. Vanderbilt. For the method please cite    !
-!                                                            !
-!  [ref] N. Marzari and D. Vanderbilt,                       !
-!        Phys. Rev. B 56 12847 (1997)                        !
-!                                                            !
-!  [ref] I. Souza, N. Marzari and D. Vanderbilt,             !
-!        Phys. Rev. B 65 035109 (2001)                       !
-!                                                            !
-!                                                            !
 ! Copyright (C) 1997-2006 Jonathan Yates, Arash Mostofi,     !
 !            Nicola Marzari, Ivo Souza, David Vanderbilt     !
 !                                                            !
@@ -323,7 +297,7 @@ contains
     restart = ' '
     call param_get_keyword('restart',found,c_value=restart)
     !post processing takes priority (must warn user of this)
-    if(postproc_setup==.true.) restart = ' '
+    if(postproc_setup) restart = ' '
 
     write_r2mn = .false.
     call param_get_keyword('write_r2mn',found,l_value=write_r2mn)
