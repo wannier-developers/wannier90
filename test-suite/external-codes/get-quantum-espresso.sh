01829ee7b1fc23a494d97687fbb78ca008cdcaf4
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2016-09-14 16:53:25 +0200

    Added ESPRESSO_ROOT setup in the script

--- test-suite/external-codes/get-quantum-espresso.sh
+++ test-suite/external-codes/get-quantum-espresso.sh
@@ -6,6 +6,8 @@ destfile=espresso-${version}.tar.gz
 url=http://qe-forge.org/gf/download/frsrelease/211/968/$destfile
 destdir=espresso
 
+export ESPRESSO_ROOT="$(pwd)/$destdir"
+
 if [ -d $destdir ]
 then
     if [ -d $destdir ]
@@ -17,3 +19,4 @@ fi
 mkdir -p $destdir
 
 wget $url --output-document=$destfile && tar xzf espresso-${version}.tar.gz -C $destdir --strip-components=1 && cd $destdir && ./configure && make -j pw pp
+
