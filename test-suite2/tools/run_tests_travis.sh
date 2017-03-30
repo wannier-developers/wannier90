b98bd7be70091812e5407758cc8db47212a6dea7
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2017-03-31 00:47:41 +0100

    Improved output from travis, and making tests verbose

--- test-suite2/tools/run_tests_travis.sh
+++ test-suite2/tools/run_tests_travis.sh
@@ -7,11 +7,18 @@ TESTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 cd "$TESTDIR"
 
 # Default: serial, no mpirun. Run these in any case
+echo "************************"
+echo "* RUNNING SERIAL TESTS *"
+echo "************************"
 ./run_tests --category=default 
 if [ "$W90BINARYPARALLEL" == "true" ]
 then
-  # If running in parallel: run also the tests in parallel
-  # I hardcode this to four, in case change it or set it as an ENV
-  # var in the .travis.yml
-  ./run_tests --category=default --numprocs=4
+    # If running in parallel: run also the tests in parallel
+    echo ""
+    echo "************************"
+    echo "* RUNNING SERIAL TESTS *"
+    echo "************************"
+    # I hardcode the numprocs to four, in case change it or set it as an ENV
+    # var in the .travis.yml
+    ./run_tests --category=default --numprocs=4
 fi
