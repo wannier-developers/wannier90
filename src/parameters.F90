4582728e9267854f8fdc35aa38de568f5dfef7c1
Junfeng Qiao <qiaojunfeng@outlook.com>
2018-08-19 11:33:12 +0800

    a small bug in header output

--- src/parameters.F90
+++ src/parameters.F90
@@ -3013,7 +3013,7 @@ contains
        else
           write(stdout,'(1x,a46,10x,a8,13x,a1)') '|  Compute Anomalous Hall Conductivity       :','       F','|'
        endif
-       if(index(berry_task,'kubo')>0) then
+       if(index(berry_task,'morb')>0) then
           write(stdout,'(1x,a46,10x,a8,13x,a1)') '|  Compute Orbital Magnetisation             :','       T','|'
        else
           write(stdout,'(1x,a46,10x,a8,13x,a1)') '|  Compute Orbital Magnetisation             :','       F','|'
