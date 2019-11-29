41f91a19af8b242523ba5375b3bb5282a42ffa41
Valerio Vitale <vitale.valerio87@gmail.com>
2019-11-29 19:18:34 +0000

    Further changes to tutorial.tex requested by G. Pizzi

--- examples/example32/generate_weights.sh
+++ examples/example32/generate_weights.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+# This script is used to extract the projectability from
+# the output of projwfc.x (proj.out) and it works only
+# for this specific system with 21 bands.
+
+# Remove temporary files if exist
+if [ -f "e.dat" ]; then
+	rm e.dat;
+fi
+
+if [ -f "p.dat" ]; then
+        rm p.dat;
+fi
+
+if [ -f "p_vs_e.dat" ]; then
+        rm p_vs_e.dat;
+fi
+
+# Check proj.out exists
+[[ -f "proj.out" ]] || { echo "proj.out not found!"; echo "Aborting!"; exit; }
+
+# Start loop over bands
 for i in {1..21}; do
         echo $i 
 	if [ $i -lt 10 ]; then 
@@ -11,4 +32,6 @@ for i in {1..21}; do
 	paste e.dat p.dat >> tmp.dat;
 done
 sort -k1n tmp.dat > p_vs_e.dat 
+
+# Clean workspace
 rm e.dat p.dat tmp.dat
