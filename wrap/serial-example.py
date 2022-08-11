19c131137afa097c34b471efb9b18fd60789786b
Barry Searle <barry.searle@stfc.ac.uk>
2022-08-11 10:04:20 +0100

    Update serial python example after retesting with current code

--- wrap/serial-example.py
+++ wrap/serial-example.py
@@ -1,5 +1,6 @@
 # Need PYTHONPATH to contain wrapper directory
 # Also need LD_LIBRARY_PATH to include it for libwannier_serial.so
+import numpy
 import wan90
 
 ftn_output = wan90.w90_helper_types.get_fortran_stdout()
@@ -7,12 +8,14 @@ ftn_output = wan90.w90_helper_types.get_fortran_stdout()
 data = wan90.w90_helper_types.lib_global_type()
 w90data = wan90.w90_helper_types.lib_w90_type()
 comm = wan90.w90_comms.w90comm_type()
-wan90.w90_helper_types.input_reader(data, w90data, "diamond", ftn_output, status, comm)
+status = wan90.w90_helper_types.input_reader(data, w90data, "diamond", ftn_output, comm)
 
 if not data.kmesh_info.explicit_nnkpts :
-    wan90.w90_helper_types.create_kmesh(data, ftn_output, status, comm)
+    status = wan90.w90_helper_types.create_kmesh(data, ftn_output, comm)
 
-import numpy
+# create dummy distribution, all done on proc 0
+kpts = numpy.zeros(data.num_kpts, dtype=numpy.int32)
+wan90.w90_helper_types.set_kpoint_distribution(data, kpts)
 
 m_matrix = numpy.zeros((data.num_wann, data.num_wann, data.kmesh_info.nntot, data.num_kpts), dtype=numpy.cdouble, order='F')
 u_matrix = numpy.zeros((data.num_wann, data.num_wann, data.num_kpts), dtype=numpy.cdouble, order='F')
@@ -28,22 +31,30 @@ if data.num_wann == data.num_bands:
     wan90.w90_helper_types.set_m_orig(w90data, m_orig)
     u_opt = numpy.zeros((1, 1, 1), dtype=numpy.cdouble, order='F')
     wan90.w90_helper_types.set_u_opt(data, u_opt)
-    wan90.w90_helper_types.overlaps(data, ftn_output, status, comm)
+    status = wan90.w90_helper_types.overlaps(data, w90data, ftn_output, comm)
+    #m_matrix_loc = numpy.zeros((data.num_wann, data.num_wann, data.kmesh_info.nntot, data.num_kpts), dtype=numpy.cdouble, order='F')
+    wan90.w90_helper_types.set_m_matrix_local(w90data, m_matrix)
 else:
+    m_matrix_loc = numpy.zeros((data.num_wann, data.num_wann, data.kmesh_info.nntot, data.num_kpts), dtype=numpy.cdouble, order='F')
+    wan90.w90_helper_types.set_m_matrix_local(w90data, m_matrix_loc)
     u_opt = numpy.zeros((data.num_bands, data.num_wann, data.num_kpts), dtype=numpy.cdouble, order='F')
     wan90.w90_helper_types.set_u_opt(data, u_opt)
     m_orig = numpy.zeros((data.num_bands, data.num_bands, data.kmesh_info.nntot, data.num_kpts), dtype=numpy.cdouble, order='F')
     wan90.w90_helper_types.set_m_orig(w90data, m_orig)
-    wan90.w90_helper_types.overlaps(data, w90data, ftn_output, status, comm)
-    wan90.w90_helper_types.disentangle(data, w90data, ftn_output, status, comm)
-    
-wan90.w90_helper_types.wannierise(data, w90data, ftn_output, status, comm)
+    status = wan90.w90_helper_types.overlaps(data, w90data, ftn_output, comm)
+    counts = numpy.zeros(1, dtype=numpy.int32)
+    counts[0]=data.num_kpts
+    displs = numpy.zeros(1, dtype=numpy.int32)
+    wan90.w90_helper_types.set_kpoint_block(data, counts, displs)
+    status = wan90.w90_helper_types.disentangle(data, w90data, ftn_output, comm)
+
+status = wan90.w90_helper_types.wannierise(data, w90data, ftn_output, comm)
 
 
-#wan90.w90_helper_types.checkpoint(data, w90data, "postwann", ftn_output, comm)
+#status = wan90.w90_helper_types.checkpoint(data, w90data, "postwann", ftn_output, comm)
 
-wan90.w90_helper_types.plot_files(data, w90data, ftn_output, status, comm)
+status = wan90.w90_helper_types.plot_files(data, w90data, ftn_output, comm)
 
-#wan90.w90_helper_types.transport(data, w90data, ftn_output, status, comm)
+#status = wan90.w90_helper_types.transport(data, w90data, ftn_output, comm)
 
 wan90.w90_helper_types.print_times(data, ftn_output)
