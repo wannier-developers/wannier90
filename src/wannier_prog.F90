3c05ccdd4eb63bb54eba1a1f111d0e5a131b249a
Jerome Jackson <jerome@neuroplug.net>
2023-05-09 16:57:26 +0100

    rationalise some variable names

--- src/wannier_prog.F90
+++ src/wannier_prog.F90
@@ -7,7 +7,7 @@ program libv2
   use mpi
 #endif
 
-  use w90_helper_types
+  use w90_library
 
   use w90_comms, only: w90_comm_type
   use w90_io, only: io_print_timings
@@ -34,17 +34,17 @@ program libv2
   integer :: stdout, stderr
   logical, pointer :: pp
   logical :: lovlp, ldsnt, lwann, lplot, ltran, need_eigvals
-  type(lib_global_type), target :: w90main
-  type(lib_w90_type), target :: w90dat
+  type(lib_common_type), target :: common_data
+  type(lib_wannier_type), target :: wannier_data
   type(w90_comm_type) :: comm
   type(w90_error_type), allocatable :: error
 
-  pp => w90dat%w90_calculation%postproc_setup
-  restart => w90dat%w90_calculation%restart
-  nw => w90main%num_wann
-  nb => w90main%num_bands
-  nk => w90main%num_kpts
-  nn => w90main%kmesh_info%nntot
+  pp => wannier_data%w90_calculation%postproc_setup
+  restart => wannier_data%w90_calculation%restart
+  nw => common_data%num_wann
+  nb => common_data%num_bands
+  nk => common_data%num_kpts
+  nn => common_data%kmesh_info%nntot
 
   ! get seedname and pp flag, if present
   call get_command_argument(1, seedname, length, ierr)
@@ -73,7 +73,7 @@ program libv2
   ! setup pplel decomp
   call mpi_comm_rank(comm%comm, rank, ierr)
   call mpi_comm_size(comm%comm, mpisize, ierr)
-  call set_parallel_comms(w90main, comm%comm)
+  call set_parallel_comms(common_data, comm%comm)
 #else
   rank = 0
   mpisize = 1
@@ -84,17 +84,17 @@ program libv2
   ! open main error file
   open (newunit=stderr, file=fn//'.werr', status="replace")
 
-  call input_reader(w90main, w90dat, fn, stdout, stderr, ierr)
+  call input_reader(common_data, wannier_data, fn, stdout, stderr, ierr)
   if (ierr /= 0) stop
 
   ! write jazzy header info
-  call w90_readwrite_write_header(w90main%physics%bohr_version_str, &
-                                  w90main%physics%constants_version_str1, &
-                                  w90main%physics%constants_version_str2, stdout) ! (not a library call)
+  call w90_readwrite_write_header(common_data%physics%bohr_version_str, &
+                                  common_data%physics%constants_version_str1, &
+                                  common_data%physics%constants_version_str2, stdout) ! (not a library call)
 
   ! setup k mesh
-  if (.not. w90main%kmesh_info%explicit_nnkpts) then
-    call create_kmesh(w90main, stdout, stderr, ierr)
+  if (.not. common_data%kmesh_info%explicit_nnkpts) then
+    call create_kmesh(common_data, stdout, stderr, ierr)
     if (ierr /= 0) stop
   endif
 
@@ -106,7 +106,7 @@ program libv2
     distk(i) = (i - 1)/nkl ! contiguous blocks with potentially fewer processes on last rank
   enddo
   ! copy distribution to library
-  call set_kpoint_distribution(w90main, distk, stdout, stderr, ierr)
+  call set_kpoint_distribution(common_data, distk, stdout, stderr, ierr)
   if (ierr /= 0) stop
 
   nkl = count(distk == rank) ! number of kpoints this rank
@@ -115,7 +115,7 @@ program libv2
   ! special branch for writing nnkp file
   if (pp) then
     ! please only invoke on rank 0
-    call write_kmesh(w90main, w90dat, fn, stdout, stderr, ierr)
+    call write_kmesh(common_data, wannier_data, fn, stdout, stderr, ierr)
     if (ierr /= 0) stop
     if (rank == 0) close (unit=stderr, status='delete')
 #ifdef MPI
@@ -124,8 +124,8 @@ program libv2
     stop
   endif
 
-  if (w90dat%lsitesymmetry) then
-    call sitesym_read(w90dat%sitesym, nb, nk, nw, fn, error, comm) ! (not a library call)
+  if (wannier_data%lsitesymmetry) then
+    call sitesym_read(wannier_data%sitesym, nb, nk, nw, fn, error, comm) ! (not a library call)
     if (allocated(error)) then
       write (stderr, *) 'failed to setup symmetry'
       deallocate (error)
@@ -138,10 +138,10 @@ program libv2
   allocate (u(nw, nw, nk))
   allocate (uopt(nb, nw, nk))
 
-  call set_m_matrix_local(w90dat, mloc)
-  !call set_m_matrix(w90dat, m)  ! we don't need global m
-  call set_u_matrix(w90main, u)
-  call set_u_opt(w90main, uopt)
+  call set_m_matrix_local(wannier_data, mloc)
+  !call set_m_matrix(wannier_data, m)  ! we don't need global m
+  call set_u_matrix(common_data, u)
+  call set_u_opt(common_data, uopt)
 
   uopt = 0.d0 ! required when not disentangling
 
@@ -156,7 +156,7 @@ program libv2
     if (rank == 0) write (stdout, '(1x,a/)') 'Starting a new Wannier90 calculation ...'
   else
     cpstatus = ''
-    call read_chkpt(w90main, w90dat, cpstatus, fn, stdout, stderr, ierr)
+    call read_chkpt(common_data, wannier_data, cpstatus, fn, stdout, stderr, ierr)
     if (ierr /= 0) stop
 
     if (restart == 'wannierise' .or. (restart == 'default' .and. cpstatus == 'postdis')) then
@@ -184,68 +184,68 @@ program libv2
       ! illegitimate restart choice, should declaim the acceptable choices
     endif
   endif
-  ltran = (ltran .or. w90dat%w90_calculation%transport)
+  ltran = (ltran .or. wannier_data%w90_calculation%transport)
   ldsnt = (ldsnt .and. (nw < nb)) ! disentanglement only needed if space reduced
 
   if (ldsnt) then
     ! setup arrays before overlap reads
     allocate (a(nb, nw, nk))
     allocate (morig(nb, nb, nn, nkl))
-    call set_a_matrix(w90dat, a)
-    call set_m_orig(w90dat, morig)
+    call set_a_matrix(wannier_data, a)
+    call set_m_orig(wannier_data, morig)
   endif
 
   ! circumstances where eigenvalues are needed are a little overcomplicated
   need_eigvals = .false.
-  need_eigvals = w90dat%w90_calculation%bands_plot
-  need_eigvals = (need_eigvals .or. w90dat%w90_calculation%fermi_surface_plot)
-  need_eigvals = (need_eigvals .or. w90dat%output_file%write_hr)
+  need_eigvals = wannier_data%w90_calculation%bands_plot
+  need_eigvals = (need_eigvals .or. wannier_data%w90_calculation%fermi_surface_plot)
+  need_eigvals = (need_eigvals .or. wannier_data%output_file%write_hr)
   need_eigvals = (need_eigvals .or. ldsnt) ! disentanglement anyway requires evals
 
   if (need_eigvals) then
     allocate (eigval(nb, nk))
-    call read_eigvals(w90main, eigval, stdout, stderr, ierr)
+    call read_eigvals(common_data, eigval, stdout, stderr, ierr)
     if (ierr /= 0) stop
-    call set_eigval(w90main, eigval)
+    call set_eigval(common_data, eigval)
   endif
 
   ! ends setup
 
   if (lovlp) then
-    call overlaps(w90main, w90dat, stdout, stderr, ierr)
+    call overlaps(common_data, wannier_data, stdout, stderr, ierr)
     if (ierr /= 0) stop
 
     if (.not. ldsnt) then
-      call projovlp(w90main, w90dat, stdout, stderr, ierr)
+      call projovlp(common_data, wannier_data, stdout, stderr, ierr)
       if (ierr /= 0) stop
     endif
   endif
 
   if (ldsnt) then
-    call disentangle(w90main, w90dat, stdout, stderr, ierr)
+    call disentangle(common_data, wannier_data, stdout, stderr, ierr)
     if (ierr /= 0) stop
-    call write_chkpt(w90main, w90dat, 'postdis', fn, stdout, stderr, ierr)
+    call write_chkpt(common_data, wannier_data, 'postdis', fn, stdout, stderr, ierr)
     if (ierr /= 0) stop
   endif
 
   if (lwann) then
-    call wannierise(w90main, w90dat, stdout, stderr, ierr)
+    call wannierise(common_data, wannier_data, stdout, stderr, ierr)
     if (ierr /= 0) stop
-    call write_chkpt(w90main, w90dat, 'postwann', fn, stdout, stderr, ierr)
+    call write_chkpt(common_data, wannier_data, 'postwann', fn, stdout, stderr, ierr)
     if (ierr /= 0) stop
   endif
 
   if (lplot) then
-    call plot_files(w90main, w90dat, stdout, stderr, ierr)
+    call plot_files(common_data, wannier_data, stdout, stderr, ierr)
     if (ierr /= 0) stop
   endif
 
   if (ltran) then
-    call transport(w90main, w90dat, stdout, stderr, ierr)
+    call transport(common_data, wannier_data, stdout, stderr, ierr)
     if (ierr /= 0) stop
   endif
 
-  call print_times(w90main, stdout)
+  call print_times(common_data, stdout)
   if (rank == 0) close (unit=stderr, status='delete')
 #ifdef MPI
   call mpi_finalize(ierr)
