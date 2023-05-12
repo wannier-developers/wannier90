a9e43fe13df1b36811ca4e30f1e33116c08fb323
Barry Searle <barry.searle@stfc.ac.uk>
2023-05-12 09:54:53 +0100

    Updates to draft pw90 library and python examples to reflect recent changes

--- wrap/mpi-example.py
+++ wrap/mpi-example.py
@@ -6,22 +6,22 @@ from mpi4py import MPI
 # Maybe should have a common name...
 import wan90mpi as wan90
 
-ftn_output = wan90.w90_helper_types.get_fortran_stdout()
-ftn_error = wan90.w90_helper_types.get_fortran_stderr()
+ftn_output = wan90.w90_library.get_fortran_stdout()
+ftn_error = wan90.w90_library.get_fortran_stderr()
 
-data = wan90.w90_helper_types.lib_global_type()
-w90data = wan90.w90_helper_types.lib_w90_type()
-comm = wan90.w90_comms.w90_comm_type()
+data = wan90.w90_library.lib_common_type()
+w90data = wan90.w90_library.lib_wannier_type()
+#comm = wan90.w90_comms.w90_comm_type()
 
-comm.comm = MPI.COMM_WORLD.py2f()
+wan90.w90_library.set_parallel_comms(data, MPI.COMM_WORLD.py2f())
 
-status = wan90.w90_helper_types.input_reader(data, w90data, "diamond", ftn_output, ftn_error, comm)
-#status = wan90.w90_helper_types.input_reader(data, w90data, "cnt55", ftn_output, ftn_error, comm)
+status = wan90.w90_library.input_reader(data, w90data, "diamond", ftn_output, ftn_error)
+#status = wan90.w90_library.input_reader(data, w90data, "cnt55", ftn_output, ftn_error)
 
 exit
 
 if not data.kmesh_info.explicit_nnkpts :
-    status = wan90.w90_helper_types.create_kmesh(data, ftn_output, ftn_error, comm)
+    status = wan90.w90_library.create_kmesh(data, ftn_output, ftn_error)
 
 # attempt to duplicate comms_array_split packing
 kpts = numpy.zeros(data.num_kpts, dtype=numpy.int32)
@@ -46,7 +46,7 @@ while k < data.num_kpts:
         if extra_pts == 0:
             pts_per_rank = pts_per_rank - 1
 
-wan90.w90_helper_types.set_kpoint_distribution(data, kpts)
+wan90.w90_library.set_kpoint_distribution(data, kpts)
 
 counts = numpy.zeros(nproc, dtype=numpy.int32)
 #displs = numpy.zeros(nproc, dtype=numpy.int32)
@@ -64,42 +64,42 @@ while k < data.num_kpts:
     cnt = cnt + 1
     k = k + 1
 counts[nproc-1] = cnt
-#wan90.w90_helper_types.set_kpoint_block(data, counts, displs)
+#wan90.w90_library.set_kpoint_block(data, counts, displs)
 
 #m_matrix = numpy.zeros((data.num_wann, data.num_wann, data.kmesh_info.nntot, data.num_kpts), dtype=numpy.cdouble, order='F')
 u_matrix = numpy.zeros((data.num_wann, data.num_wann, data.num_kpts), dtype=numpy.cdouble, order='F')
 
-#wan90.w90_helper_types.set_m_matrix(w90data, m_matrix)
-wan90.w90_helper_types.set_u_matrix(data, u_matrix)
+#wan90.w90_library.set_m_matrix(w90data, m_matrix)
+wan90.w90_library.set_u_matrix(data, u_matrix)
 
 m_matrix_loc = numpy.zeros((data.num_wann, data.num_wann, data.kmesh_info.nntot, counts[my_proc]), dtype=numpy.cdouble, order='F')
-wan90.w90_helper_types.set_m_matrix_local(w90data, m_matrix_loc)
+wan90.w90_library.set_m_matrix_local(w90data, m_matrix_loc)
 
 if data.num_wann == data.num_bands:
     u_opt = numpy.zeros((1, 1, 1), dtype=numpy.cdouble, order='F')
-    wan90.w90_helper_types.set_u_opt(data, u_opt)
-    status = wan90.w90_helper_types.overlaps(data, w90data, ftn_output, ftn_error, comm)
+    wan90.w90_library.set_u_opt(data, u_opt)
+    status = wan90.w90_library.overlaps(data, w90data, ftn_output, ftn_error)
 else:
     a_matrix = numpy.zeros((data.num_bands, data.num_wann, data.num_kpts), dtype=numpy.cdouble, order='F')
-    wan90.w90_helper_types.set_a_matrix(w90data, a_matrix)
+    wan90.w90_library.set_a_matrix(w90data, a_matrix)
     u_opt = numpy.zeros((data.num_bands, data.num_wann, data.num_kpts), dtype=numpy.cdouble, order='F')
-    wan90.w90_helper_types.set_u_opt(data, u_opt)
+    wan90.w90_library.set_u_opt(data, u_opt)
     m_orig = numpy.zeros((data.num_bands, data.num_bands, data.kmesh_info.nntot, data.num_kpts), dtype=numpy.cdouble, order='F')
-    wan90.w90_helper_types.set_m_orig(w90data, m_orig)
-    status = wan90.w90_helper_types.overlaps(data, w90data, ftn_output, ftn_error, comm)
-    status = wan90.w90_helper_types.disentangle(data, w90data, ftn_output, ftn_error, comm)
+    wan90.w90_library.set_m_orig(w90data, m_orig)
+    status = wan90.w90_library.overlaps(data, w90data, ftn_output, ftn_error)
+    status = wan90.w90_library.disentangle(data, w90data, ftn_output, ftn_error)
     if status == 1:
         exit
 
-status = wan90.w90_helper_types.wannierise(data, w90data, ftn_output, ftn_error, comm)
+status = wan90.w90_library.wannierise(data, w90data, ftn_output, ftn_error)
 
-#wan90.w90_helper_types.checkpoint(data, w90data, "postwann", ftn_output, ftn_error, comm)
+#wan90.w90_library.checkpoint(data, w90data, "postwann", ftn_output, ftn_error)
 
-#wan90.w90_helper_types.transport(helper, w90data, ftn_output, ftn_error, status, comm)
+#wan90.w90_library.transport(helper, w90data, ftn_output, ftn_error, status)
 
 #print (data.num_wann)
 
-#wan90.w90_helper_types.plot_files(data, w90data, ftn_output, ftn_error, status, comm)
+#wan90.w90_library.plot_files(data, w90data, ftn_output, ftn_error, status)
 
 if my_proc == 0:
-    wan90.w90_helper_types.print_times(data, ftn_output)
+    wan90.w90_library.print_times(data, ftn_output)
