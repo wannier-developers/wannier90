8c54a46d5cb203e9f51d6f37469c24bd47cc608c
Junfeng Qiao <qiaojunfeng@outlook.com>
2025-01-19 22:17:19 +0100

    Remove C++ reference
    Make sure wannier90.h is a pure C header file.

--- src/wannier90.h
+++ src/wannier90.h
@@ -64,29 +64,29 @@ void w90_set_option_text(w90_data blob, char* key, char* data) {
     CFI_establish(&desc1, data, CFI_attribute_other, CFI_type_char, strlen(data), 0, NULL);
     w90_set_option_text_f(blob, &desc, &desc1);
 };
-void w90_input_setopt_f(w90_data, CFI_cdesc_t*, int);
-void w90_input_setopt(w90_data blob, char* seed, int& ierr) {
+void w90_input_setopt_f(w90_data, CFI_cdesc_t*, int*);
+void w90_input_setopt(w90_data blob, char* seed, int* ierr) {
     CFI_cdesc_t desc;
     CFI_establish(&desc, seed, CFI_attribute_other, CFI_type_char, strlen(seed), 0, NULL);
-    ierr = 0;
+    *ierr = 0;
     w90_input_setopt_f(blob, &desc, ierr);
 }
-void w90_create(w90_data&);
-void w90_delete(w90_data&);
-void w90_disentangle(w90_data, int&);
+void w90_create(w90_data*);
+void w90_delete(w90_data*);
+void w90_disentangle(w90_data, int*);
 void w90_get_centres(w90_data, void*);
 void w90_get_gkpb(w90_data, void*);
 void w90_get_nn(w90_data, void*);
 void w90_get_nnkp(w90_data, void*);
 void w90_get_spreads(w90_data, void*);
-void w90_input_reader(w90_data, int&);
-void w90_overlaps(w90_data, int&);
-void w90_project_overlap(w90_data, int&);
+void w90_input_reader(w90_data, int*);
+void w90_overlaps(w90_data, int*);
+void w90_project_overlap(w90_data, int*);
 void w90_set_eigval(w90_data, void*);
 void w90_set_m_local(w90_data, void*);
 void w90_set_u_matrix(w90_data, void*);
 void w90_set_u_opt(w90_data, void*);
-void w90_wannierise(w90_data, int&);
+void w90_wannierise(w90_data, int*);
 #ifdef MPI_VERSION
 #include <mpi.h>
 void w90_set_comm(w90_data blob, MPI_Comm comm){
