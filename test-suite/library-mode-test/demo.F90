c6cb0b3eff6801d4b0d350d527c3dda286795d57
Jerome Jackson <jerome@neuroplug.net>
2026-07-27 14:28:14 +0100

    fallback mpi interface for simple cmake setup (lib test)

--- test-suite/library-mode-test/demo.F90
+++ test-suite/library-mode-test/demo.F90
@@ -27,20 +27,16 @@
 
 program ok
 
+! fallback to f90 mpi interface for this test's simplified cmake setup
 #ifdef MPI08
   use mpi_f08
-#endif
-#ifdef MPI90
+#else
   use mpi
 #endif
   use w90_library
   use w90_library_extra, only: overlaps
 
   implicit none
-
-#ifdef MPIH
-  include 'mpif.h'
-#endif
 
   complex(8), allocatable :: m_matrix(:, :, :, :)
   complex(8), allocatable :: u_matrix(:, :, :)
